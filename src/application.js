e8c7f9f3d07a6b9d76e2835d33960bbc6d852dee
AlexMak <alekseymak178@gmail.com>
2020-06-07 22:50:32 +0300

    upgrade submit listener

--- src/application.js
+++ src/application.js
@@ -37,8 +37,8 @@ export default () => {
   });
   const watchedUrl = onChange(state.form, (_path, url) => {
     const errors = validate(url);
-    console.log(errors);
     if (errors.length === 0) {
+      console.log('flex');
       watchedButton.status = false;
       inputField.classList.remove('is-invalid');
       feedback.classList.remove('text-danger');
