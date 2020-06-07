f53381ce18b537014e189d1a8e47a9996d6c8361
AlexMak <alekseymak178@gmail.com>
2020-06-07 13:28:53 +0300

    add input validation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,37 @@
+import * as yup from 'yup';
+
+const onChange = require('on-change');
+
 const component = () => {
-  document.querySelector('.container').innerHTML = 'Flex!';
+  const schema = yup.object().shape({
+    url: yup.string().required().url(),
+  });
+
+  const state = {
+    form: {
+      url: '',
+    },
+  };
+  const feedback = document.querySelector('.feedback');
+  const [form] = document.forms;
+  const inputField = form.querySelector('.form-control');
+  const submitButton = form.querySelector('input[type="submit"]');
+  const watchedState = onChange(state.form, (_path, url) => {
+    schema.validate({ url })
+      .then(() => {
+        submitButton.disabled = false;
+        inputField.classList.remove('is-invalid');
+      })
+      .catch((err) => {
+        const [error] = err.errors;
+        inputField.classList.add('is-invalid');
+        feedback.classList.add('text-danger');
+        feedback.textContent = error;
+      });
+  });
+  inputField.addEventListener('input', (e) => {
+    e.preventDefault();
+    watchedState.url = e.target.value;
+  });
 };
 document.body.appendChild(component());
