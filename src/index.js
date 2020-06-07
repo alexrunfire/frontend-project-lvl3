34981c6b602fa4f440702e8d78b4d3855e373a45
AlexMak <alekseymak178@gmail.com>
2020-06-07 17:53:56 +0300

    upgrade input validation

--- src/index.js
+++ src/index.js
@@ -2,34 +2,39 @@ import * as yup from 'yup';
 
 const onChange = require('on-change');
 
-const component = () => {
-  const schema = yup.object().shape({
-    url: yup.string().required().url(),
-  });
+const schema = yup.object().shape({
+  url: yup.string().required().url(),
+});
 
+const feedback = document.querySelector('.feedback');
+const [form] = document.forms;
+const inputField = form.querySelector('.form-control');
+const submitButton = form.querySelector('input[type="submit"]');
+
+const validate = (url) => {
+  schema.validate({ url })
+    .then(() => [])
+    .catch((err) => err.errors);
+};
+
+const component = () => {
   const state = {
     form: {
       url: '',
     },
   };
-  const feedback = document.querySelector('.feedback');
-  const [form] = document.forms;
-  const inputField = form.querySelector('.form-control');
-  const submitButton = form.querySelector('input[type="submit"]');
+
   const watchedState = onChange(state.form, (_path, url) => {
-    schema.validate({ url })
-      .then(() => {
-        submitButton.disabled = false;
-        inputField.classList.remove('is-invalid');
-      })
-      .catch((err) => {
-        if (err) {
-          const [error] = err.errors;
-          inputField.classList.add('is-invalid');
-          feedback.classList.add('text-danger');
-          feedback.textContent = error;
-        }
-      });
+    const errors = validate(url);
+    if (errors.length === 0) {
+      submitButton.disabled = false;
+      inputField.classList.remove('is-invalid');
+    } else {
+      const [error] = errors;
+      inputField.classList.add('is-invalid');
+      feedback.classList.add('text-danger');
+      feedback.textContent = error;
+    }
   });
   inputField.addEventListener('input', (e) => {
     e.preventDefault();
