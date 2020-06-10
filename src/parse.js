c31517c7c39ce8cf67c1215558801825298edc61
AlexMak <alekseymak178@gmail.com>
2020-06-10 10:19:21 +0300

    upgrade rss parsing

--- src/parse.js
+++ src/parse.js
@@ -1,5 +1,4 @@
 export default (response) => {
   const domparser = new DOMParser();
-  const xmlDoc = domparser.parseFromString(response.data, 'text/html').toString();
-  return domparser.parseFromString(xmlDoc, 'text/html');
+  return domparser.parseFromString(response.data, 'text/html');
 };
