67c1789a1dd84d7c618d23a8223d5ab1fd1b9411
AlexMak <alekseymak178@gmail.com>
2020-06-10 10:38:19 +0300

    upgrade rss parsing

--- src/proceedDoc.js
+++ src/proceedDoc.js
@@ -3,8 +3,9 @@ export default (doc) => {
     const title = doc.querySelector('title').textContent;
     const description = doc.querySelector('description').textContent;
     const items = [...doc.querySelectorAll('item')].reduce((acc, item) => {
-      const itemTitle = item.querySelector('title').childNodes[0].data;
-      console.log(item.querySelector('title').childNodes[0].textContent);
+      const itemTitle = item.querySelector('title').childNodes[0];
+      console.log(itemTitle);
+      console.log(typeof itemTitle);
       const pubDate = item.querySelector('pubdate').textContent;
       const link = item.querySelector('link').textContent;
       return [...acc, { itemTitle, pubDate, link }];
