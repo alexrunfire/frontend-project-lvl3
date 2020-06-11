32edfc7ff9834af37653d5100b4bd4d6bb505558
AlexMak <alekseymak178@gmail.com>
2020-06-11 23:16:22 +0300

    create view.js

--- src/application.js
+++ src/application.js
@@ -1,14 +1,12 @@
 import * as yup from 'yup';
-import i18next from 'i18next';
+import axios from 'axios';
 import parse from './parse';
 import proceedDoc from './proceedDoc';
-import resources from './en';
+import {
+  watchedForm, watchedFeedback, watchedRows, inputField, form,
+} from './view';
 
-
-const onChange = require('on-change');
-
-const axios = require('axios');
-
+const rssUrls = [];
 const proxy = {
   url: () => 'cors-anywhere.herokuapp.com',
 };
@@ -16,13 +14,6 @@ const proxy = {
 const schema = yup.object().shape({
   url: yup.string().required().url(),
 });
-
-const feedbackField = document.querySelector('.feedback');
-const [form] = document.forms;
-const inputField = form.querySelector('input');
-const submitButton = form.querySelector('button');
-const rssItems = document.querySelector('.rss-items');
-const rssLinks = document.querySelector('.rss-links');
 
 const validate = (url) => {
   try {
@@ -33,126 +24,49 @@ const validate = (url) => {
   }
 };
 
-export default () => {
-  i18next.init({
-    lng: 'en',
-    debug: true,
-    resources,
-  });
-
-  const state = {
-    form: {
-      submitButton: null,
-      emptyInput: null,
-      validStatus: null,
-    },
-    feedback: {
-      value: null,
-      textDanger: null,
-      textSuccess: null,
-      rssExists: null,
-      empty: null,
-    },
-    rssUrls: [],
-    rssRows: {
-      heads: null,
-      items: [],
-    },
-  };
-
-  const watchedForm = onChange(state.form, (path, value) => {
-    if (path === 'submitButton') {
-      submitButton.disabled = value;
-    } else if (path === 'validStatus' && !value) {
-      inputField.classList.add('is-invalid');
-    } else if (path === 'validStatus' && value) {
-      inputField.classList.remove('is-invalid');
-    } else if (path === 'emptyInput' && value) {
-      inputField.value = '';
-    }
-  });
-
-  const watchedFeedback = onChange(state.feedback, (path, value) => {
-    if (path === 'value') {
-      feedbackField.textContent = value;
-    } else if (path === 'textDanger' && value) {
-      feedbackField.classList.remove('text-success');
-      feedbackField.classList.add('text-danger');
-    } else if (path === 'textDanger' && !value) {
-      feedbackField.classList.remove('text-success');
-      feedbackField.classList.remove('text-danger');
-    } else if (path === 'textSuccess' && value) {
-      feedbackField.classList.add('text-success');
-      feedbackField.textContent = i18next.t('rssLoaded');
-    } else if (path === 'rssExists' && value) {
-      feedbackField.textContent = i18next.t('rssExists');
-    } else if (path === 'empty' && value) {
-      feedbackField.textContent = '';
-    }
-  });
-  const watchedRows = onChange(state.rssRows, (path, currentValue, previousValue) => {
-    if (path === 'heads') {
-      const div = document.createElement('div');
-      const a = document.createElement('a');
-      a.setAttribute('href', currentValue.headLink);
-      a.textContent = `${currentValue.title} (${currentValue.description})`;
-      div.append(a);
-      rssItems.prepend(div);
-    } else if (path === 'items') {
-      rssLinks.innerHTML = '';
-      [...currentValue, ...previousValue].forEach((item) => {
-        const div = document.createElement('div');
-        const a = document.createElement('a');
-        a.setAttribute('href', item.link);
-        a.textContent = item.itemTitle;
-        div.append(a);
-        rssLinks.append(div);
-      });
-    }
-  });
-
-  const validateUrl = (url) => {
-    const errors = validate(url);
-    if (errors.length === 0) {
+const validateUrl = (url) => {
+  const errors = validate(url);
+  if (errors.length === 0) {
+    watchedForm.submitButton = false;
+    watchedForm.validStatus = true;
+    watchedFeedback.empty = true;
+    watchedFeedback.textDanger = false;
+  } else {
+    watchedForm.submitButton = true;
+    watchedForm.validStatus = false;
+    const [error] = errors;
+    watchedFeedback.value = error;
+    watchedFeedback.textDanger = true;
+  }
+};
+const checkDoc = (doc, url) => {
+  const parserError = doc.querySelector('parsererror');
+  if (parserError) {
+    watchedFeedback.value = parserError.textContent;
+    watchedFeedback.textDanger = true;
+  } else {
+    const rssData = proceedDoc(doc);
+    watchedRows.heads = rssData.head;
+    watchedRows.items = rssData.items;
+    watchedFeedback.textSuccess = true;
+    watchedForm.emptyInput = true;
+    rssUrls.push(url);
+  }
+};
+const makeGetRequest = (url) => {
+  axios.get(url)
+    .then((response) => {
       watchedForm.submitButton = false;
-      watchedForm.validStatus = true;
-      watchedFeedback.empty = true;
-      watchedFeedback.textDanger = false;
-    } else {
-      watchedForm.submitButton = true;
-      watchedForm.validStatus = false;
-      const [error] = errors;
-      watchedFeedback.value = error;
+      const doc = parse(response);
+      checkDoc(doc, url);
+    })
+    .catch((err) => {
+      watchedForm.submitButton = false;
+      watchedFeedback.value = err.message;
       watchedFeedback.textDanger = true;
-    }
-  };
-  const checkDoc = (doc, url) => {
-    const parserError = doc.querySelector('parsererror');
-    if (parserError) {
-      watchedFeedback.value = parserError.textContent;
-      watchedFeedback.textDanger = true;
-    } else {
-      const rssData = proceedDoc(doc);
-      watchedRows.heads = rssData.head;
-      watchedRows.items = rssData.items;
-      watchedFeedback.textSuccess = true;
-      watchedForm.emptyInput = true;
-      state.rssUrls.push(url);
-    }
-  };
-  const makeGetRequest = (url) => {
-    axios.get(url)
-      .then((response) => {
-        watchedForm.submitButton = false;
-        const doc = parse(response);
-        checkDoc(doc, url);
-      })
-      .catch((err) => {
-        watchedForm.submitButton = false;
-        watchedFeedback.value = err.message;
-        watchedFeedback.textDanger = true;
-      });
-  };
+    });
+};
+export default () => {
   inputField.addEventListener('input', (e) => {
     e.preventDefault();
     watchedForm.emptyInput = false;
@@ -165,7 +79,7 @@ export default () => {
     e.preventDefault();
     watchedForm.submitButton = true;
     const url = `https://${proxy.url()}/${inputField.value}`;
-    if (state.rssUrls.includes(url)) {
+    if (rssUrls.includes(url)) {
       watchedForm.submitButton = false;
       watchedForm.validStatus = false;
       watchedFeedback.textDanger = true;
---
+++ src/view.js
@@ -0,0 +1,90 @@
+import i18next from 'i18next';
+import resources from './en';
+
+const onChange = require('on-change');
+
+const feedbackField = document.querySelector('.feedback');
+const [form] = document.forms;
+const inputField = form.querySelector('input');
+const submitButton = form.querySelector('button');
+const rssItems = document.querySelector('.rss-items');
+const rssLinks = document.querySelector('.rss-links');
+
+i18next.init({
+  lng: 'en',
+  debug: true,
+  resources,
+});
+
+const state = {
+  form: {
+    submitButton: null,
+    emptyInput: null,
+    validStatus: null,
+  },
+  feedback: {
+    value: null,
+    textDanger: null,
+    textSuccess: null,
+    rssExists: null,
+    empty: null,
+  },
+  rssRows: {
+    heads: null,
+    items: [],
+  },
+};
+
+const watchedForm = onChange(state.form, (path, value) => {
+  if (path === 'submitButton') {
+    submitButton.disabled = value;
+  } else if (path === 'validStatus' && !value) {
+    inputField.classList.add('is-invalid');
+  } else if (path === 'validStatus' && value) {
+    inputField.classList.remove('is-invalid');
+  } else if (path === 'emptyInput' && value) {
+    inputField.value = '';
+  }
+});
+
+const watchedFeedback = onChange(state.feedback, (path, value) => {
+  if (path === 'value') {
+    feedbackField.textContent = value;
+  } else if (path === 'textDanger' && value) {
+    feedbackField.classList.remove('text-success');
+    feedbackField.classList.add('text-danger');
+  } else if (path === 'textDanger' && !value) {
+    feedbackField.classList.remove('text-success');
+    feedbackField.classList.remove('text-danger');
+  } else if (path === 'textSuccess' && value) {
+    feedbackField.classList.add('text-success');
+    feedbackField.textContent = i18next.t('rssLoaded');
+  } else if (path === 'rssExists' && value) {
+    feedbackField.textContent = i18next.t('rssExists');
+  } else if (path === 'empty' && value) {
+    feedbackField.textContent = '';
+  }
+});
+const watchedRows = onChange(state.rssRows, (path, currentValue, previousValue) => {
+  if (path === 'heads') {
+    const div = document.createElement('div');
+    const a = document.createElement('a');
+    a.setAttribute('href', currentValue.headLink);
+    a.textContent = `${currentValue.title} (${currentValue.description})`;
+    div.append(a);
+    rssItems.prepend(div);
+  } else if (path === 'items') {
+    rssLinks.innerHTML = '';
+    [...currentValue, ...previousValue].forEach((item) => {
+      const div = document.createElement('div');
+      const a = document.createElement('a');
+      a.setAttribute('href', item.link);
+      a.textContent = item.itemTitle;
+      div.append(a);
+      rssLinks.append(div);
+    });
+  }
+});
+export {
+  watchedForm, watchedFeedback, watchedRows, inputField, form,
+};
