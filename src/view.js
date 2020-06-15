36fc1a80ccf0b4ba87b7a09c769299974151dbd5
AlexMak <alekseymak178@gmail.com>
2020-06-15 15:09:23 +0300

    add console to fix prblems

--- src/view.js
+++ src/view.js
@@ -56,6 +56,7 @@ const findNewArticles = (currentValue, previousValue) => _.reduce(currentValue,
 const makeItems = (currentValue, previousValue) => {
   console.log(currentValue);
   const newArticles = findNewArticles(currentValue, previousValue);
+  console.log(newArticles);
   if (rssLinks.childNodes.length === 0) {
     newArticles.forEach((article) => {
       const { articleLink, articleTitle } = article;
